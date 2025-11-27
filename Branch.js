ac196a6b5d24255e70b9541fa1c03fd960754471
Sunny Ranpise <sunnyranpise04@gmail.com>
2025-11-27 13:43:23 +0530

    Added files

---
+++ Branch.js
@@ -0,0 +1,31 @@
+// What is Git branch?
+
+// -> A Git branch is like a separate workspace where you can make changes without affecting your main project.
+
+// It is like creating a copy of the main project and experimenting on that copy.
+
+
+// Branch commands :
+
+// 1) git branch : Used to check branch
+
+// 2) git checkout -b <-New branch name-> : Used to create new branch.
+
+// 3) git branch -M name : Used to rename branch.
+
+// 4) git checkout <-branch name-> : Used to go in different branch.
+
+// 5) git branch -d <-branch name-> : Used to delete branch.
+
+
+// Now lets see how branches work on real projects.
+
+
+// There are two ways to merge : 
+
+// 1) Pull request (A pull request (PR) is a way to ask for your changes to be added (merged) into another branch on GitHub.)
+
+// 2) git merge : Used to merge 2 branches. (main and other) 
+
+// Note : Pull request and pull command are different things in Git.
+
